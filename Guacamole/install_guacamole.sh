f605e93ed56cf83be62f5f5b55d7f8983a4f46af
AZOGg35 <stan.skate2001@gmail.com>
2021-09-24 14:34:46 +0200

    Modif

---
+++ Guacamole/install_guacamole.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+clear
+
+# Récupération de l'IP du serveur
+SERVER_IP=$(hostname --all-ip-addresses | cut -d " " -f1)
+
+# Détection de l'OS
+source /etc/os-release
+OS=$ID
+
+if [ $OS == "debian" ] || [ $OS == "kali" ] || [ $OS == "ubuntu" ]; then
+	    echo "OS supporté"
+    else
+	        echo "OS non supporté"
+fi
+
+if [ $OS == "kali" ]; then
+	    OS="debian"
+fi
+
+# Installation des dépendances et de docker
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$OS/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-compose
+systemctl enable docker
+systemctl start docker
+
+mkdir /guacamole
+cp docker-compose.yml /guacamole
+cd /guacamole
+
+#Lancement du docker-compose.yml
+docker-compose up -d
+
+clear
+tput bold; tput setaf 7; echo "LISTES DES CONTAINERS EN COURS : "
+tput setaf 3; echo ""
+docker container ls
+echo ""
+tput setaf 7; echo "--------------------------------------------------"
+tput bold; tput setaf 7; echo "           => INSTALLATION TERMINEE <=           "
+tput setaf 7; echo ""
+tput setaf 7; echo "              Lien : $SERVER_IP:8080              "
+tput setaf 7; echo "    Identifiant : guacadmin | MDP : guacadmin     "
+tput setaf 7; echo ""
+tput bold; tput setaf 6; echo "                   By AZOGg                      "
+tput bold; tput setaf 6; echo "                   azogg.gitlab.com/my_doc       "
+tput setaf 7; echo "--------------------------------------------------"
+tput setaf 2; echo ""
